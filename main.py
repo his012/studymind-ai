faf2a9a797da450cf9434ba49043292922e7df3e
hisgits <farhanyou012@gmail.com>
2026-07-21 01:29:07 +0500

    Fix CORS

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ app = FastAPI(title="StudyMind AI", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 class ChatRequest(BaseModel):
